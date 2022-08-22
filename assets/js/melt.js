4915178614224b5a1ac5c519c54f710a043f0c87
ʀɪᴛᴇꜱʜ ᴋᴜꜱʜᴡᴀʜᴀ <67673777+aiedrow@users.noreply.github.com>
2022-08-22 20:19:54 +0530

    Create melt.js

---
+++ assets/js/melt.js
@@ -0,0 +1,9 @@
+/*!
+ * Melt v2.1 (https://by.aiedrow.in/melt/)
+ * Copyright 2019-2022 Aiedrow.in
+ * Licensed under MIT (https://github.com/aiedrow/Melt/blob/master/LICENSE)
+ !*/
+
+function a(){
+
+}
